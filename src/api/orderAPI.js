5339982abb7bad0ad105bf85a6ef899b8b76adce
KIM SUMIN <gms09065@naver.com>
2025-11-21 15:21:34 +0900

    fix/#34: typo mistake

--- src/api/orderAPI.js
+++ src/api/orderAPI.js
@@ -1,30 +1,30 @@
 // src/api/orderAPI.js
 
-import axiosInstance from "./axiosInstance";
+import axiosInstance from './axiosInstance';
 
 //결제 전 주문 정보 조회
 export async function getCheckoutInfo(orderItemIds) {
-    const reponse = await axiosInstance.post('/api/v1/orders/pre-check', {
-        checkoutItemIds: orderItemIds,
-    });
+  const response = await axiosInstance.post('/api/v1/orders/pre-check', {
+    checkoutItemIds: orderItemIds,
+  });
 
-    if (response.status === 200) {
-        return response.data;
-    }
+  if (response.status === 200) {
+    return response.data;
+  }
 
-    throw new Error('결제 정보 조회 실패');
+  throw new Error('결제 정보 조회 실패');
 }
 
 // 주문 생성
 export async function createOrder(addressId, orderItemIds) {
-    const response = await axiosInstance.post('/api/v1/orders', {
-        addressId,
-        orderItemIds,
-    });
+  const response = await axiosInstance.post('/api/v1/orders', {
+    addressId,
+    orderItemIds,
+  });
 
-    if (response.status === 200) {
-        return response.data;
-    }
+  if (response.status === 200) {
+    return response.data;
+  }
 
-    throw new Error('주문 생성 실패');
+  throw new Error('주문 생성 실패');
 }
